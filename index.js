6b16ba8330c94872db6b1f16e7fde1199e79f407
suman <suman@gomeplus.com>
2018-12-24 15:48:13 +0800

    init project

---
+++ index.js
@@ -0,0 +1,15 @@
+let Koa = require('koa');
+let Router = require('koa-router');
+let static = require('koa-static');
+let path = require('path');
+let fs = require('fs');
+let app = new Koa();
+let router = new Router();
+
+app.use(static(path.join(__dirname, 'www')));
+
+app.on('error', (err)=>{
+  console.log(err);
+})
+app.use(router.routes());
+app.listen(3001);
